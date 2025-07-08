a9f80dca84712634158b960cb65d5b43b8023276
Thabang <phoshokothabang1@gmail.com>
2025-07-08 20:19:32 +0200

    fix: update task2 to add a second conditional

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,9 @@
 const { argv } = require('node:process');
 
-if (argv.length > 2) {
-    console.log('Argument found');
+if (argv.length === 3) {
+    console.log("Argument found");
+} else if (argv.length > 3) {
+    console.log("Arguments found");
 } else {
     console.log("No argument");
 }
